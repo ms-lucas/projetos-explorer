6435547e9447901f52b895aec4c8a528620b13e6
Lucas Moreira <ms.lucasmoreira@gmail.com>
2024-02-22 22:10:09 -0300

    Projeto SPA Finalizado

---
+++ vanilla/projetos/projeto-spa/javascript/main.js
@@ -0,0 +1,16 @@
+import { Router } from "./router.js";
+
+const router = new Router();
+
+console.log(router)
+
+router.add("/", "/pages/home.html");
+router.add("/about", "/pages/about.html");
+router.add("/contact", "/pages/contact.html");
+router.add("/not-found", "/pages/not-found.html");
+
+router.handle();
+
+window.onpopstate = () => router.handle();
+window.route = () => router.route();
+ 
